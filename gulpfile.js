eae6ed26c448974736bc7d7a96bba9f42f54b6eb
earelin <javier.carriba@telegraph.co.uk>
2022-03-13 18:39:38 +0000

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 'use strict'
 
-const { series, parallel } = require('gulp')
+const { series } = require('gulp')
 const util = require( 'util' )
 
 const exec = util.promisify( require( 'child_process' ).exec )
 
 const BUILD_COMMAND = "npm run build"
 const CLEAN_COMMAND = "npm run clean"
+const TEST_COMMAND = "npm test"
 
 function build() {
   return Promise.all([
@@ -22,6 +23,17 @@ function clean() {
   ])
 }
 
+function test() {
+  const environment = process.env
+  environment.CI = true
+
+  return Promise.all([
+    exec(TEST_COMMAND, {cwd: `${process.cwd()}/admin`, env: environment}),
+    exec(TEST_COMMAND, {cwd: `${process.cwd()}/site`, env: environment})
+  ])
+}
+
 exports.build = build
 exports.clean = clean
+exports.test = test
 exports.default = series(clean, build)
