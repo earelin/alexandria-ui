dcd7c7618578a24f883ed780822ea1ae6c084fa2
earelin <javier.carriba@telegraph.co.uk>
2022-03-13 18:47:31 +0000

    Adding CI

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const util = require( 'util' )
 const exec = util.promisify( require( 'child_process' ).exec )
 
 const BUILD_COMMAND = "npm run build"
+const c = "npm ci"
 const CLEAN_COMMAND = "npm run clean"
 const TEST_COMMAND = "npm test"
 
@@ -13,6 +14,13 @@ function build() {
   return Promise.all([
     exec(BUILD_COMMAND, {cwd: `${process.cwd()}/admin`}),
     exec(BUILD_COMMAND, {cwd: `${process.cwd()}/site`})
+  ])
+}
+
+function buildCi() {
+  return Promise.all([
+    exec(BUILD_CI_COMMAND, {cwd: `${process.cwd()}/admin`}),
+    exec(BUILD_CI_COMMAND, {cwd: `${process.cwd()}/site`})
   ])
 }
 
@@ -34,6 +42,7 @@ function test() {
 }
 
 exports.build = build
+exports.ci = buildCi
 exports.clean = clean
 exports.test = test
 exports.default = series(clean, build)
