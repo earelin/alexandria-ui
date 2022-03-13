9d975cdc50d50e78eed459f2907c0a8df0fd2309
Xavier Carriba <javier.carriba@telegraph.co.uk>
2022-03-13 19:03:24 +0000

    Adding CI (#1)

--- gulpfile.js
+++ gulpfile.js
@@ -6,34 +6,45 @@ const util = require( 'util' )
 const exec = util.promisify( require( 'child_process' ).exec )
 
 const BUILD_COMMAND = "npm run build"
+const BUILD_CI_COMMAND = "npm ci"
 const CLEAN_COMMAND = "npm run clean"
 const TEST_COMMAND = "npm test"
 
 function build() {
-  return Promise.all([
-    exec(BUILD_COMMAND, {cwd: `${process.cwd()}/admin`}),
-    exec(BUILD_COMMAND, {cwd: `${process.cwd()}/site`})
-  ])
+  return applyCommandToAllSubprojects(BUILD_COMMAND)
+}
+
+function buildCi() {
+  return applyCommandToAllSubprojects(BUILD_CI_COMMAND)
 }
 
 function clean() {
-  return Promise.all([
-    exec(CLEAN_COMMAND, {cwd: `${process.cwd()}/admin`}),
-    exec(CLEAN_COMMAND, {cwd: `${process.cwd()}/site`})
-  ])
+  return applyCommandToAllSubprojects(CLEAN_COMMAND)
 }
 
 function test() {
   const environment = process.env
-  environment.CI = true
+  environment.CI = 'true'
 
+  return applyCommandToAllSubprojectsWithEnv(TEST_COMMAND, environment)
+}
+
+function applyCommandToAllSubprojects(command) {
   return Promise.all([
-    exec(TEST_COMMAND, {cwd: `${process.cwd()}/admin`, env: environment}),
-    exec(TEST_COMMAND, {cwd: `${process.cwd()}/site`, env: environment})
+    exec(command, {cwd: `${process.cwd()}/admin`}),
+    exec(command, {cwd: `${process.cwd()}/site`})
+  ])
+}
+
+function applyCommandToAllSubprojectsWithEnv(command, environment) {
+  return Promise.all([
+    exec(command, {cwd: `${process.cwd()}/admin`, env: environment}),
+    exec(command, {cwd: `${process.cwd()}/site`, env: environment})
   ])
 }
 
 exports.build = build
+exports.ci = buildCi
 exports.clean = clean
 exports.test = test
 exports.default = series(clean, build)
