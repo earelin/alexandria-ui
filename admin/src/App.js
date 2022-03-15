e0a9597f84753eb5a32ab8547f00311b72e59f5a
earelin <javier.carriba@telegraph.co.uk>
2022-03-15 19:18:14 +0000

    Adding pure-css library

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='Layout' className='pure-g'>
+        <div className='pure-u-1-3'>First</div>
+        <div className='pure-u-1-3'>Second</div>
+        <div className='pure-u-1-3'>Third</div>
+      </div>
     </div>
   );
 }
