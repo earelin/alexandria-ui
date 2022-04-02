9a636e19cdc324611a406f403a131fafa5d1784d
earelin <javier.carriba@telegraph.co.uk>
2022-04-02 19:36:11 +0100

    Improving performance

---
+++ admin/src/user/UserList.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react'
+import {getUsersList} from './UserService'
+
+export default function UserList() {
+  const [userList, setUserList] = useState({content: []});
+
+  useEffect(
+    () => {
+      getUsersList()
+        .then(userData => setUserList(userData))
+    }, [setUserList]
+  )
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList.content.map(user => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
